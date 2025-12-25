0d7ab0b36a6c131449ac5cdeddd07eab5234914e
soum-anum <soumya.anum@gmail.com>
2025-12-26 00:57:54 +0530

    "added-main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import App from './App';
 import { HashRouter } from "react-router-dom";
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
-
